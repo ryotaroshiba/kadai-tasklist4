34c4274457827985443a1ee88fa8ff205059a925
jiro.kirameki <ec2-user@ip-172-31-22-207.us-east-2.compute.internal>
2018-05-31 05:30:25 +0000

    add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,7 +4,8 @@
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
-    <p>{{ $task->content }}</p>
+    <p>ステータス: {{ $task->status }}</p>
+    <p>メッセージ{{ $task->content }}</p>
     
     {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id]) !!}
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
