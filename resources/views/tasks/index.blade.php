96cab3bcca08ccfd8dc819c68bac0eba1f7d5426
jiro.kirameki <ec2-user@ip-172-31-22-207.us-east-2.compute.internal>
2018-06-01 00:19:40 +0000

    kadai-tasklist

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -24,7 +24,6 @@
             </tbody>
         </table>
     @endif
-     {!! link_to_route('tasks.create', '新規タスクの投稿') !!}
+     {!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
 
 @endsection
-{!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
