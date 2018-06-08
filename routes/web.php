a5dcc1cda5be9316047dfb5411411904f18593cb
jiro.kirameki <ec2-user@ip-172-31-22-207.us-east-2.compute.internal>
2018-06-08 01:47:20 +0000

    kadai-tasklist6

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::resource('tasks', 'TasksController');
 //    return view('welcome');
 //});
 
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
